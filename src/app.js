d620038e335c6f387a7558b78250b21a2ae97625
Dustin Neighly <neighlyd@gmail.com>
2019-02-28 16:52:38 -0800

    Rendering functions in component

--- src/app.js
+++ src/app.js
@@ -15,26 +15,35 @@ const template = (
     </div>
 );
 
-const user = {
-    name: 'Dustin Neighly',
-    age: 36,
-    loc: 'Seattle'
-}
+let count = 0;
+const addOne = () => {
+    count++;
+    renderCounterApp();
+};
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+};
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
 
-const getLocation = (loc) => {
-    if (loc) {
-        return <p>Location: {loc}</p>
-    } 
-}
-
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-        {getLocation(user.loc)}
-    </div>
-);
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+            
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+}
+
+renderCounterApp();
