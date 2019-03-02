ed4fbb2e63a96ea0c39d204bf823c6cdd0194511
Dustin Neighly <neighlyd@gmail.com>
2019-03-01 16:23:18 -0800

    Began styling with SCSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            {props.subtitle && <h2>Put your life in the hands of a computer</h2>}
+const Header = (props) => (
+    <div className='header'>
+        <div className='container'>
+            <h1 className='header__title'>{props.title}</h1>
+            {props.subtitle && <h2 className='header__subtitle'>Put your life in the hands of a computer</h2>}
         </div>
-    );
-};
+    </div>
+);
 
 Header.defaultProps = {
     title: 'Indecision App'
